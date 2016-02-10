072a0d159870da849ce4aab133849b5a1ce5e831
Ratt <ee>
2016-02-10 23:55:24 +0100

    stricter string check

--- plugins/gif.py
+++ plugins/gif.py
@@ -6,36 +6,29 @@ gifs = []
 
 def process_message(data):
 
-    if channel in data['channel']:
-
-        text = data['text']
-        channel = data['channel']
-         
-        if '.gif' in text:
-
-            if 'reload' in text: 
-
-                reload()
-                print('smileFile has been reloaded')
+    text = data['text']
+    channel = data['channel']
+     
+    if '.gif' in text:
+        text = text[5:]
+        print(text)
+        if 'reload' in text: 
+            reload()
+            print('smileFile has been reloaded')
+        
+        elif 'list' in text:
+            outputText = gifs[0][0]
+            for gif in gifs[1:]:
+                outputText += ', '+gif[0]
+    
+            outputs.append([channel,"{}".format(outputText)])
+    
+        else:
+            for gif in gifs:
+                if gif[0] == text:
+                    print('Sending gif {} to {}'.format(gif[0],channel))
+                    outputs.append([channel,"{}".format(gif[1])])
             
-            elif 'list' in text:
-
-                outputText = gifs[0][0]
-
-                for gif in gifs[1:]:
-                    outputText += ', '+gif[0]
-
-                outputs.append([channel,"{}".format(outputText)])
-
-            else:
-
-                for gif in gifs:
-                    
-                    if gif[0] in text:
-
-                        print('Sending gif {} to {}'.format(gif[0],channel))
-                        outputs.append([channel,"{}".format(gif[1])])
-                    
 
 def reload():
 
