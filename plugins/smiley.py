ed13458a4b1dca8ab1dbb62331b4ef540b1bed79
Ratt <ee>
2016-02-10 13:58:37 +0100

    it\'s everywhere now

--- plugins/smiley.py
+++ plugins/smiley.py
@@ -5,17 +5,19 @@ outputs = []
 smilies = []
 
 def process_message(data):
-    if "C0K2UNFB2" in data['channel']:
-
-        if '.reload' in data['text']:
+#    if "C0K2UNFB2" in data['channel']:
+    text = data['text']
+    channel = data['channel']
+     
+    if '.reload' in text or '.smile' in text:
+        if '.reload' in text: 
             reload()
             print('smileFile has been reloaded')
-
-        elif '.smile' in data['text']:
+    
+        elif '.smile' in text:
             randomNumber = numpy.random.random_integers(len(smilies)-1)
-            print('Sending smile number {}'.format(randomNumber))
-            outputs.append([data['channel'],\
-                    "{}".format(smilies[randomNumber])])
+            print('Sending smile {} to {}'.format(smilies[randomNumber],channel))
+            outputs.append([channel,"{}".format(smilies[randomNumber])])
 
 def reload():
    del smilies[:]
