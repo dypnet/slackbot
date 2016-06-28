755e2d71f5e0e144bb05a51dc857020baceadf8a
hati <ha@ti.fnatt>
2016-06-28 11:25:32 +0200

    3123

---
+++ BOT/THEbot.py
@@ -0,0 +1,188 @@
+#!../slackBot/bin/python
+import sys
+sys.path.insert(0,"plugins/")
+import os
+import time
+import logging
+#import test
+
+from slackclient import SlackClient
+
+def dbg(debug_string):
+    if debug:
+        print(debug_string)
+        #logging.info(debug_string)
+
+class RtmBot():
+    def __init__(self, token):
+        self.last_ping = 0
+        self.token = token
+        self.bot_plugins = []
+        self.slack_client = None
+    def connect(self):
+        """Convenience method that creates Server instance"""
+        self.slack_client = SlackClient(self.token)
+        self.slack_client.rtm_connect()
+    def start(self):
+        self.connect()
+        while True:
+            for reply in self.slack_client.rtm_read():
+                self.input(reply)
+            self.crons()
+            self.output()
+            self.autoping()
+            time.sleep(.1)
+    def autoping(self):
+        #hardcode the interval to 3 seconds
+        now = int(time.time())
+        if now > self.last_ping + 3:
+            self.slack_client.server.ping()
+            self.last_ping = now
+    def input(self, data):
+        if "type" in data:
+            function_name = "process_" + data["type"]
+            dbg("got {}".format(function_name))
+            for plugin in self.bot_plugins:
+                plugin.register_jobs()
+                plugin.do(function_name, data)
+    def output(self):
+        for plugin in self.bot_plugins:
+            limiter = False
+            for output in plugin.do_output():
+                channel = self.slack_client.server.channels.find(output[0])
+                if len(output[1]) > 0 :
+                    if limiter == True:
+                        time.sleep(.1)
+                        limiter = False
+                    message = str(output[1])
+                    channel.send_message("{}".format(message))
+                    limiter = True
+    def crons(self):
+        for plugin in self.bot_plugins:
+            plugin.do_jobs()
+    def load_plugins(self):
+        for plugin in glob.glob(directory+'/plugins/*'):
+            sys.path.insert(0, plugin)
+            sys.path.insert(0, directory+'/plugins/')
+        for plugin in glob.glob(directory+'/plugins/*.py') + glob.glob(directory+'/plugins/*/*.py'):
+            logging.info(plugin)
+            name = plugin.split('/')[-1][:-3]
+#            try:
+            self.bot_plugins.append(Plugin(name))
+#            except:
+#                print "error loading plugin %s" % name
+
+class Plugin():
+    def __init__(self, name, plugin_config={}):
+        self.name = name
+        self.jobs = []
+        self.module = __import__(name)
+        self.register_jobs()
+        self.outputs = []
+
+    def register_jobs(self):
+        if 'crontable' in dir(self.module):
+            for interval, function in self.module.crontable:
+                self.jobs.append(Job(interval, eval("self.module."+function)))
+            logging.info(self.module.crontable)
+            self.module.crontable = []
+        else:
+            self.module.crontable = []
+
+    def do(self, function_name, data):
+        if function_name in dir(self.module):
+            #this makes the plugin fail with stack trace in debug mode
+            if debug:
+                try:
+                    eval("self.module."+function_name)(data)
+                except:
+                    dbg("problem in module {} {}".format(function_name, data))
+            else:
+                eval("self.module."+function_name)(data)
+        if "catch_all" in dir(self.module):
+            try:
+                self.module.catch_all(data)
+            except:
+                dbg("problem in catch all")
+    def do_jobs(self):
+        for job in self.jobs:
+            job.check()
+    def do_output(self):
+        output = []
+
+        while True:
+            if 'outputs' in dir(self.module):
+                if len(self.module.outputs) > 0:
+                    logging.info("output from {}".format(self.module))
+                    
+                    output.append(self.module.outputs.pop(0))
+                else:
+                    break
+            else:
+                self.module.outputs = []
+        return output
+
+class Job():
+    def __init__(self, interval, function):
+        self.function = function
+        self.interval = interval
+        self.lastrun = 0
+    def __str__(self):
+        return "{} {} {}".format(self.function, self.interval, self.lastrun)
+    def __repr__(self):
+        return self.__str__()
+    def check(self):
+        if self.lastrun + self.interval < time.time():
+            if debug:
+                try:
+                    self.function()
+                except:
+                    dbg("problem")
+            else:
+                self.function()
+            self.lastrun = time.time()
+            pass
+
+class UnknownChannel(Exception):
+    pass
+
+
+def main_loop():
+    logging.info(directory)
+    try:
+        bot.start()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except:
+        logging.exception('OOPS')
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-c',
+        '--config',
+        help='Full path to config file.',
+        metavar='path'
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    directory = os.path.dirname(sys.argv[0])
+    f = open('token.txt','r')
+    token = f.readline().strip()
+    f.close()
+    debug = True
+
+    bot = RtmBot(token)
+    bot.bot_plugins.append(Plugin('smiley'))
+    bot.bot_plugins.append(Plugin('gif'))
+
+    site_plugins = []
+    files_currently_downloading = []
+    job_hash = {}
+
+    main_loop()
+
---
+++ BOT/plugins/gif.py
@@ -0,0 +1,53 @@
+import time
+import numpy
+crontable = []
+outputs = []
+gifs = []
+random = []
+
+def process_message(data):
+
+    text = data['text']
+    channel = data['channel']
+
+    if '.gif' in text:
+        text = text[5:]
+        print(text)
+        if 'reload' in text:
+            reload()
+            print('gifFiles has been reloaded')
+
+        elif 'list' in text:
+            outputText = gifs[0][0]
+            for gif in gifs[1:]:
+                outputText += ', '+gif[0]
+            outputs.append([channel,"{}".format(outputText)])
+
+        elif 'random' in text:
+            randomNumber = numpy.random.random_integers(len(random)-1)
+            print('Sending random gif number {}'.format(randomNumber))
+            outputs.append([channel,"{}".format(random[randomNumber])])
+
+        else:
+            for gif in gifs:
+                if gif[0] == text:
+                    print('Sending gif {} to {}'.format(gif[0],channel))
+                    outputs.append([channel,"{}".format(gif[1])])
+
+
+
+def reload():
+
+   del gifs[:]
+   gifFile = open('plugins/reactionGifFile','r')
+   gifRandFile = open('plugins/randomGifFile','r')
+
+   for gif in gifFile:
+       gifs.append(gif.strip().split(','))
+
+   for gif in gifRandFile: 
+       random.append(gif.strip())
+
+   gifFile.close()
+
+reload()
